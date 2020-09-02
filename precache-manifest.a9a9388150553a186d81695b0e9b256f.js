17a63122a0b286e685e73e2d59dbad2bcb1095da
Aman Mishra <aman21.mishra@gmail.com>
2020-09-02 08:43:54 +0530

    Updates

---
+++ precache-manifest.a9a9388150553a186d81695b0e9b256f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b54101582418a51113f91326ad611f1",
+    "url": "/memeify/index.html"
+  },
+  {
+    "revision": "f3e68b9b2f19f5145bd6",
+    "url": "/memeify/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a073f0f184bd0353f073",
+    "url": "/memeify/static/js/2.55b2415f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memeify/static/js/2.55b2415f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3e68b9b2f19f5145bd6",
+    "url": "/memeify/static/js/main.2e95d9c9.chunk.js"
+  },
+  {
+    "revision": "ce2ef09ac232c525df05",
+    "url": "/memeify/static/js/runtime-main.029561c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/memeify/static/media/logo.5d5d9eef.svg"
+  }
+]);
