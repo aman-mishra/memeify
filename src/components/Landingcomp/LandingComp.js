ede3f238b2b4dfda3b3513b906c89d008d403750
Aman Mishra <aman21.mishra@gmail.com>
2020-09-06 16:49:54 +0530

    Landingcomponent made

---
+++ src/components/Landingcomp/LandingComp.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./Landingcomp.css";
+
+import name from "../../assets/Images/Memeifyname.png"
+import line from "../../assets/Images/Yourstudies.png"
+import quote_no from "../../assets/Images/quote_no.png"
+import quote_yes from "../../assets/Images/quote_yes.png"
+import meme_no from "../../assets/Images/meme_no.png"
+import meme_yes from "../../assets/Images/meme_yes.png"
+
+const Landingcomp = (props) => {
+    return(
+        <div className="">
+           <table className="aligncont">
+                <tr>
+                <td>
+                    <tr>
+                    <img src={name} alt="Memeify" />
+                    </tr>
+                    <tr>
+                    <img src={line} alt="Your Studies" />
+                    </tr>
+                </td>
+                <td>
+                    <table>
+                        <tr>
+                        <td>
+                            <img src={meme_no} alt="drake no" />
+                        </td>
+                        <td>
+                            <img src={quote_no} alt="reading 10 pages of notes" />
+                        </td>
+                        </tr>
+                        <tr>
+                        <td>
+                            <img src={meme_yes} alt="Drake Yes" />
+                        </td>
+                        <td>
+                            <img src={quote_yes} alt="Going through 100 meme of the same subject !!" />
+                        </td>
+                        </tr>
+                    </table>
+                    </td>
+                    </tr>
+          </table>
+        </div>
+    );
+};
+
+export default Landingcomp;
